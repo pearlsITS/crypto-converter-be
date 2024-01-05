6b484fcc4df4fb929ef561293cdc642424e6e723
pearlsITS <126848667+pearlsITS@users.noreply.github.com>
2024-01-05 12:11:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.get('/',() => return "Hello Crypto.!")
 app.use('/api', cryptoRoutes);
 
 app.listen(PORT, () => {
