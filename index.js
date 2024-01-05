9bed0631ec8165c83d9bc9c9a7f34a52ed97543a
pearlsITS <126848667+pearlsITS@users.noreply.github.com>
2024-01-05 12:49:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cryptoRoutes = require('./routes/cryptoRoutes');
-const bodyParser = require("body-parser");
 const cors = require("cors");
-// require('dotenv').config()
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 app.get('/',(req,res) => res.send( "Hello Crypto.!"))
 app.use('/api', cryptoRoutes);
