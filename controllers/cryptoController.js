5643045bcd4131b870d93119ab79ec0e7c620511
pearlsITS <126848667+pearlsITS@users.noreply.github.com>
2024-01-05 12:35:11 +0530

    Update cryptoController.js

--- controllers/cryptoController.js
+++ controllers/cryptoController.js
@@ -3,6 +3,7 @@ const errorHandler = require("../utils/errorHandler");
 
 const getTopCryptos = async (req, res) => {
   try {
+    console.log('url',process.env.BASE_URL);
     const response = await axios.get(`${process.env.BASE_URL}/coins/markets`, {
       params: {
         vs_currency: "usd",
