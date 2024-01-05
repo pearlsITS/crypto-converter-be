5d1f017645292b8ab89af30f9447eae288c4b9b5
pearlsITS <126848667+pearlsITS@users.noreply.github.com>
2024-01-05 13:07:39 +0530

    Update errorHandler.js

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (res, error) => {
   let statusCode = 500;
-  if (error?.status.error_code) {
-    statusCode = error?.status?.error_code;
+  if (error.status.error_code) {
+    statusCode = error.status.error_code;
   }
   res.status(statusCode).json({ error });
 };
